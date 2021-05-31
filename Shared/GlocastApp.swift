1e4bd99c36868941dfae9635ea2cc371e27afbe6
David Nordstrom <mr.nordstrom77@gmail.com>
2021-05-31 19:03:43 +0200

    Initial Commit

---
+++ Shared/GlocastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlocastApp.swift
+//  Shared
+//
+//  Created by Macbook on 31/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct GlocastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
